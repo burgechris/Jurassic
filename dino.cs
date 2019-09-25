b48c15f70d236c7c598a2e89eb089e7de92a6610
Chris Burge and Joel Stockamp <joel@>
2019-09-25 10:31:57 -0700

    add initial files and dino constructor.

---
+++ dino.cs
@@ -0,0 +1,25 @@
+using System;using System;
+using System.Collections.Generic;
+
+namespace Park
+{
+  class Dinosaur
+  {
+    private string _species;
+    private int _id;
+    private int _weight;
+    private bool _carnivore;
+    private int _hungerLevel;
+    private string _location;
+  }
+
+  public class Dinosaur(string species, int id, int weight, bool carnivore, int _hungerLevel, string location)
+  {
+    _species = species;
+    _id = id;
+    _weight = weight;
+    _carnivore = carnivore;
+    _hungerLevel = _hungerLevel;
+    _location = TypeLoadException;
+  }
+}
